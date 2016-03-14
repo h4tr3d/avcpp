791216ab2fd9f1a2dc6457c9a7bbcab954a2da18
Alexander Drozdov <adrozdoff@gmail.com>
2016-03-14 16:45:44 +1000

    Improuve XxxFormat family classes
    - Check that given codec supported by the format
    - Search InputFormat/OutputFormat in ctors
    - Add noexcept to the most methods

--- src/format.cpp
+++ src/format.cpp
@@ -1,9 +1,29 @@
+#include "codec.h"
 #include "format.h"
 
 namespace av {
 
+namespace internal {
+
+bool codec_supported(const AVCodecTag * const *codecTag, const FmtCodec &codec)
+{
+    if (!codecTag) // any codec supported
+        return true;
+
+    unsigned int tag;
+    return !!av_codec_get_tag2(codecTag, codec.id(), &tag);
+}
+
+} // ::internal
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-bool InputFormat::setFormat(const std::string &name) {
+InputFormat::InputFormat(const std::string &name) noexcept
+{
+    setFormat(name);
+}
+
+bool InputFormat::setFormat(const std::string &name) noexcept
+{
     if (!name.empty())
         m_raw = av_find_input_format(name.c_str());
     else
@@ -12,7 +32,12 @@ bool InputFormat::setFormat(const std::string &name) {
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-bool OutputFormat::setFormat(const std::string &name, const std::string &url, const std::string &mime)
+OutputFormat::OutputFormat(const std::string &name, const std::string &url, const std::string &mime) noexcept
+{
+    setFormat(name, url, mime);
+}
+
+bool OutputFormat::setFormat(const std::string &name, const std::string &url, const std::string &mime) noexcept
 {
     if (name.empty() && url.empty() && mime.empty())
         m_raw = nullptr;
@@ -23,12 +48,14 @@ bool OutputFormat::setFormat(const std::string &name, const std::string &url, co
     return m_raw;
 }
 
-AVCodecID OutputFormat::defaultVideoCodecId() const
+
+AVCodecID OutputFormat::defaultVideoCodecId() const noexcept
 {
     return RAW_GET(video_codec, AV_CODEC_ID_NONE);
 }
 
-AVCodecID OutputFormat::defaultAudioCodecId() const
+
+AVCodecID OutputFormat::defaultAudioCodecId() const noexcept
 {
     return RAW_GET(audio_codec, AV_CODEC_ID_NONE);
 }
@@ -36,10 +63,7 @@ AVCodecID OutputFormat::defaultAudioCodecId() const
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 OutputFormat guessOutputFormat(const std::string &name, const std::string &url, const std::string &mime)
 {
-    OutputFormat result;
-    result.setFormat(name, url, mime);
-    return result;
+    return {name, url, mime};
 }
-
 
 } // namespace av
--- src/format.h
+++ src/format.h
@@ -7,32 +7,44 @@
 
 namespace av {
 
+using FmtCodec = class Codec;
+namespace internal {
+bool codec_supported(const AVCodecTag *const *codecTag, const FmtCodec &codec);
+} // ::internal
+
 template<typename T>
 struct Format : public FFWrapperPtr<T>
 {
     using FFWrapperPtr<T>::FFWrapperPtr;
 
-    const char* name() const {
+    const char* name() const noexcept {
         return RAW_GET(name, nullptr);
     }
 
-    const char* longName() const {
+    const char* longName() const noexcept {
         return RAW_GET(long_name, nullptr);
     }
 
-    int32_t flags() const {
+    int32_t flags() const noexcept {
         return RAW_GET(flags, 0);
     }
 
-    bool isFlags(int32_t flags) const {
+    bool isFlags(int32_t flags) const noexcept {
         if (m_raw) {
             return (m_raw->flags & flags);
         }
         return false;
     }
 
-    void setFormat(const T* format) {
+    void setFormat(const T* format) noexcept {
         reset(format);
+    }
+
+    bool codecSupported(const class Codec &codec) const noexcept
+    {
+        if (!m_raw)
+            return false;
+        return internal::codec_supported(m_raw->codec_tag, codec);
     }
 
 protected:
@@ -46,7 +58,11 @@ public:
     using Format<AVInputFormat>::Format;
     using Format<AVInputFormat>::setFormat;
 
-    bool setFormat(const std::string& name);
+    InputFormat() = default;
+
+    InputFormat(const std::string& name) noexcept;
+
+    bool setFormat(const std::string& name) noexcept;
 
 };
 
@@ -57,12 +73,18 @@ public:
     using Format<AVOutputFormat>::Format;
     using Format<AVOutputFormat>::setFormat;
 
+    OutputFormat() = default;
+
+    OutputFormat(const std::string& name,
+                 const std::string& url  = std::string(),
+                 const std::string& mime = std::string()) noexcept;
+
     bool        setFormat(const std::string& name,
                           const std::string& url  = std::string(),
-                          const std::string& mime = std::string());
+                          const std::string& mime = std::string()) noexcept;
 
-    AVCodecID   defaultVideoCodecId() const;
-    AVCodecID   defaultAudioCodecId() const;
+    AVCodecID   defaultVideoCodecId() const noexcept;
+    AVCodecID   defaultAudioCodecId() const noexcept;
 };
 
 
