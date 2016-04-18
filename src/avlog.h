32a6acecb20e78b7f495062c7191458192b6d0b9
Alexander S. Drozdov <adrozdoff@gmail.com>
2016-04-18 17:24:54 +1000

    Logger macrosses clean up

--- src/avlog.h
+++ src/avlog.h
@@ -6,18 +6,18 @@
 #include "ffmpeg.h"
 
 /// Use null context as logging context
-#define null_log(level, format, args...)     av_log(nullptr, level, "%s: " format,  __PRETTY_FUNCTION__, ##args)
+#define null_log(level, format, ...)     av_log(nullptr, level, "%s: " format,  __PRETTY_FUNCTION__, ##__VA_ARGS__)
 
 /// Use context ptr as logging context
-#define ptr_log(level, format, args...)      av_log(m_raw, level, "%s: " format,  __PRETTY_FUNCTION__, ##args)
+#define ptr_log(level, format, ...)      av_log(m_raw, level, "%s: " format,  __PRETTY_FUNCTION__, ##__VA_ARGS__)
 
 /// Use context referenct as logging context
-#define ref_log(level, format, args...)      av_log(&m_raw, level, "%s: " format,  __PRETTY_FUNCTION__, ##args)
+#define ref_log(level, format, ...)      av_log(&m_raw, level, "%s: " format,  __PRETTY_FUNCTION__, ##__VA_ARGS__)
 
 /// Use specific context as logging context for pretty logging
-#define ctx_log(ctx, level, format, args...) av_log(ctx, level, "%s: " format,  __PRETTY_FUNCTION__, ##args)
+#define ctx_log(ctx, level, format, ...) av_log(ctx, level, "%s: " format,  __PRETTY_FUNCTION__, ##__VA_ARGS__)
 
 /// Default in-class logger
-#define fflog(level, format, args...) _log(level, "%s: " format, __PRETTY_FUNCTION__, ##args)
+#define fflog(level, format, ...) _log(level, "%s: " format, __PRETTY_FUNCTION__, ##__VA_ARGS__)
 
 #endif // LOG_H
