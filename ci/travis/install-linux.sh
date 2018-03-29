ba4c97bb5e3181cfda0c6774c85e4122b1ecc8ef
Alexander Drozdov <adrozdoff@gmail.com>
2018-03-29 11:49:06 +1000

    Fix Travis Linux setup script

--- ci/travis/install-linux.sh
+++ ci/travis/install-linux.sh
@@ -11,7 +11,7 @@ export MAKEFLAGS="-j${JOBS}"
     CMAKE_VERSION_MINOR=1
     CMAKE_VERSION_FULL=${CMAKE_VERSION_BASE}.${CMAKE_VERSION_MINOR}
     wget -c http://www.cmake.org/files/v${CMAKE_VERSION_BASE}/cmake-${CMAKE_VERSION_FULL}.tar.gz
-    tar -xvzf cmake-${CMAKE_VERSION_FULL}.tar.gz
+    tar -xzf cmake-${CMAKE_VERSION_FULL}.tar.gz
     cd cmake-${CMAKE_VERSION_FULL}/
     ./configure --prefix=/usr
     make
@@ -22,7 +22,7 @@ export MAKEFLAGS="-j${JOBS}"
 (
     sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
     sudo apt-get -qq update
-    sudo apt-get install -y g++5 g++6 g++7
+    sudo apt-get install -y 'g\+\+-5' 'g\+\+-6' 'g\+\+-7'
 )
 
 # FFmpeg
