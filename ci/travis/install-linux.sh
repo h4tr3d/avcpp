39dcd1c68f53ecd710a21f069c50766e0ad4da07
Alexander Drozdov <adrozdoff@gmail.com>
2018-03-29 13:37:24 +1000

    Travis: speed up Linux builds

--- ci/travis/install-linux.sh
+++ ci/travis/install-linux.sh
@@ -6,19 +6,33 @@ JOBS=$(cat /proc/cpuinfo | grep '^processor' | wc -l)
 export MAKEFLAGS="-j${JOBS}"
 
 # CMake
-(
-    CMAKE_VERSION_BASE=3.10
-    CMAKE_VERSION_MINOR=1
-    CMAKE_VERSION_FULL=${CMAKE_VERSION_BASE}.${CMAKE_VERSION_MINOR}
-    wget -c http://www.cmake.org/files/v${CMAKE_VERSION_BASE}/cmake-${CMAKE_VERSION_FULL}.tar.gz
-    tar -xzf cmake-${CMAKE_VERSION_FULL}.tar.gz
-    cd cmake-${CMAKE_VERSION_FULL}/
-    ./configure --prefix=/usr
-    make
-    sudo make install
-)
+echo "Prepare CMake"
+build_cmake()
+{
+    local CMAKE_VERSION_BASE=3.11
+    local CMAKE_VERSION_MINOR=0
+    local CMAKE_VERSION_FULL=${CMAKE_VERSION_BASE}.${CMAKE_VERSION_MINOR}
+    local CMAKE_ARCH=x86_64
+
+    #wget -c http://www.cmake.org/files/v${CMAKE_VERSION_BASE}/cmake-${CMAKE_VERSION_FULL}.tar.gz
+    #tar -xzf cmake-${CMAKE_VERSION_FULL}.tar.gz
+    #cd cmake-${CMAKE_VERSION_FULL}/
+    #./configure --prefix=/usr
+    #make
+    #sudo make install
+
+    wget -c https://cmake.org/files/v${CMAKE_VERSION_BASE}/cmake-${CMAKE_VERSION_FULL}-Linux-${CMAKE_ARCH}.tar.gz
+    tar -xzf cmake-${CMAKE_VERSION_FULL}-Linux-${CMAKE_ARCH}.tar.gz
+    export PATH=$(pwd)/cmake-${CMAKE_VERSION_FULL}-Linux-${CMAKE_ARCH}/bin:$PATH
+    cmake --version
+}
+
+build_cmake
+
+exit 0
 
 # Newer GCC
+echo "Prepare GCC"
 (
     sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
     sudo apt-get -qq update
@@ -27,6 +41,7 @@ export MAKEFLAGS="-j${JOBS}"
 
 # FFmpeg
 # - https://launchpad.net/~jonathonf/+archive/ubuntu/ffmpeg-3
+echo "Prepare FFmpeg"
 (
     sudo add-apt-repository ppa:jonathonf/ffmpeg-3 -y
     sudo add-apt-repository ppa:jonathonf/tesseract -y
