15a512c2cf5670c44658c70c44aa086f50e20b8f
Alexander Drozdov <adrozdoff@gmail.com>
2018-03-29 10:47:05 +1000

    Update Travis Linux configuration script

--- ci/travis/install-linux.sh
+++ ci/travis/install-linux.sh
@@ -2,14 +2,27 @@
 
 set -ex
 
+JOBS=$(cat /proc/cpuinfo | grep '^processor' | wc -l)
+export MAKEFLAGS="-j${JOBS}"
+
 # CMake
 (
-    wget http://www.cmake.org/files/v3.10/cmake-3.10.1.tar.gz
-    tar -xvzf cmake-3.4.1.tar.gz
-    cd cmake-3.4.1/
+    CMAKE_VERSION_BASE=3.10
+    CMAKE_VERSION_MINOR=1
+    CMAKE_VERSION_FULL=${CMAKE_VERSION_BASE}.${CMAKE_VERSION_MINOR}
+    wget -c http://www.cmake.org/files/v${CMAKE_VERSION_BASE}/cmake-${CMAKE_VERSION_FULL}.tar.gz
+    tar -xvzf cmake-${CMAKE_VERSION_FULL}.tar.gz
+    cd cmake-${CMAKE_VERSION_FULL}/
     ./configure --prefix=/usr
     make
     sudo make install
+)
+
+# Newer GCC
+(
+    sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+    sudo apt-get -qq update
+    sudo apt-get install -y g++5 g++6 g++7
 )
 
 # FFmpeg
@@ -17,15 +30,14 @@ set -ex
 (
     sudo add-apt-repository ppa:jonathonf/ffmpeg-3 -y
     sudo add-apt-repository ppa:jonathonf/tesseract -y
-    sudo apt update && sudo apt upgrade
-    sudo apt install libavcodec-dev \
-                     libavdevice-dev \
-                     libavfilter-dev \
-                     libavformat-dev \
-                     libavresample-dev \
-                     libavutil-dev \
-                     libpostproc-dev \
-                     libswresample-dev \
-                     libswscale-dev
+    sudo apt-get -qq update
+    sudo apt-get install -y libavcodec-dev \
+                            libavdevice-dev \
+                            libavfilter-dev \
+                            libavformat-dev \
+                            libavresample-dev \
+                            libavutil-dev \
+                            libpostproc-dev \
+                            libswresample-dev \
+                            libswscale-dev
 )
-
