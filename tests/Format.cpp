h4tr3d/avcpp

commit 491b56a796a32a5ff009b5d8ddcd4a543244c414

Adjust to ffmpeg avformat 59 API changes. (#75)
Format functions are returning those as const pointers now.
There is ff_const59 that could have been (ab)used for transition period,
however it may disappear without warning in future.

This change should be backward compatible.

src/format.h
@@ -52,12 +52,11 @@ protected:
     using FFWrapperPtr<T>::m_raw;
 };
 
-
-class InputFormat : public Format<AVInputFormat>
+class InputFormat : public Format<const AVInputFormat>
 {
 public:
-    using Format<AVInputFormat>::Format;
-    using Format<AVInputFormat>::setFormat;
+    using Format<const AVInputFormat>::Format;
+    using Format<const AVInputFormat>::setFormat;
 
     InputFormat() = default;
 
@@ -68,11 +67,11 @@ public:
 };
 
 
-class OutputFormat : public Format<AVOutputFormat>
+class OutputFormat : public Format<const AVOutputFormat>
 {
 public:
-    using Format<AVOutputFormat>::Format;
-    using Format<AVOutputFormat>::setFormat;
+    using Format<const AVOutputFormat>::Format;
+    using Format<const AVOutputFormat>::setFormat;
 
     OutputFormat() = default;
 

commit b90ad47922fd6d152f091b7f46bba06991990988

Fix compilation error introduced by PR #75
Relative to PR #76

src/ffmpeg.h
@@ -31,6 +31,11 @@ extern "C" {
 #endif
 }
 
+// Related to FF_API_AVIOFORMAT
+#ifndef AVCPP_API_AVIOFORMAT
+#define AVCPP_API_AVIOFORMAT   (LIBAVFORMAT_VERSION_MAJOR < 59)
+#endif
+
 // Compat level
 
 // avcodec

src/format.h
@@ -1,10 +1,16 @@
-#ifndef AV_FORMAT_H
-#define AV_FORMAT_H
+#pragma once
 
 #include <memory>
 #include <string>
 
 #include "ffmpeg.h"
+
+// ff_const59 is not part of public API
+#if AVCPP_API_AVIOFORMAT
+#define avcpp_format_const
+#else
+#define avcpp_format_const const
+#endif
 
 namespace av {
 
@@ -37,8 +43,8 @@ struct Format : public FFWrapperPtr<T>
         return false;
     }
 
-    void setFormat(const T* format) noexcept {
-        reset(format);
+    void setFormat(avcpp_format_const T* format) noexcept {
+        FFWrapperPtr<T>::reset(format);
     }
 
     bool codecSupported(const class Codec &codec) const noexcept
@@ -52,11 +58,11 @@ protected:
     using FFWrapperPtr<T>::m_raw;
 };
 
-class InputFormat : public Format<const AVInputFormat>
+class InputFormat : public Format<avcpp_format_const AVInputFormat>
 {
 public:
-    using Format<const AVInputFormat>::Format;
-    using Format<const AVInputFormat>::setFormat;
+    using Format<avcpp_format_const AVInputFormat>::Format;
+    using Format<avcpp_format_const AVInputFormat>::setFormat;
 
     InputFormat() = default;
 
@@ -67,11 +73,11 @@ public:
 };
 
 
-class OutputFormat : public Format<const AVOutputFormat>
+class OutputFormat : public Format<avcpp_format_const AVOutputFormat>
 {
 public:
-    using Format<const AVOutputFormat>::Format;
-    using Format<const AVOutputFormat>::setFormat;
+    using Format<avcpp_format_const AVOutputFormat>::Format;
+    using Format<avcpp_format_const AVOutputFormat>::setFormat;
 
     OutputFormat() = default;
 
@@ -94,4 +100,3 @@ OutputFormat guessOutputFormat(const std::string& name,
 
 } // namespace av
 
-#endif // AV_FORMAT_H

tests/CMakeLists.txt
@@ -12,7 +12,8 @@ target_link_libraries(test_main PUBLIC Catch2::Catch2)
 add_executable(test_executor
     Frame.cpp
     AvDeleter.cpp
-    Packet.cpp)
+    Packet.cpp
+    Format.cpp)
 target_link_libraries(test_executor PUBLIC Catch2::Catch2 test_main avcpp::avcpp)
 
 list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/../catch2/contrib")

tests/Format.cpp
@@ -1,0 +1,32 @@
+#include <catch2/catch.hpp>
+
+#include <vector>
+
+#include "format.h"
+#include "codec.h"
+
+#ifdef _MSC_VER
+# pragma warning (disable : 4702) // Disable warning: unreachable code
+#endif
+
+
+TEST_CASE("Format Core functionality", "[Format]")
+{
+    SECTION("guessEncodingCodec()") {
+        auto out_format = av::guessOutputFormat("matroska");
+        CHECK(out_format.raw());
+
+        auto codec = av::guessEncodingCodec(out_format, "matroska", "out.mkv", nullptr, AVMEDIA_TYPE_VIDEO);
+        CHECK(codec.raw());
+    }
+
+    SECTION("setFormat") {
+        auto tmp_format = av::guessOutputFormat("matroska");
+        CHECK(tmp_format.raw());
+
+        av::OutputFormat of;
+        of.setFormat(tmp_format.raw());
+
+        CHECK(tmp_format.raw() == of.raw());
+    }
+}

tests/meson.build
@@ -22,6 +22,7 @@ tests = [
     'Frame',
     'AvDeleter',
     'Packet',
+    'Format',
 ]
 
 #create all the tests